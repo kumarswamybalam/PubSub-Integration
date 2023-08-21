8c9d153e13eaacb64644592dd7787729d3c4e1fd
Kumar Swamy Balam <kumarswamybalam@Kumars-MacBook-Air.local>
2023-08-21 10:31:29 -0500

    added header filters

--- src/main/java/com/balam/pubsub/integration/controller/PubSubPublisherController.java
+++ src/main/java/com/balam/pubsub/integration/controller/PubSubPublisherController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 @Slf4j
@@ -22,6 +23,11 @@ public class PubSubPublisherController {
     @PostMapping("/publish")
     public String publishMessage(@RequestBody DemoMessage demoMessage,
                                                     @RequestHeader Map<String, String> headers) throws ExecutionException, InterruptedException {
-        return publisherService.publishToDemoTopic(demoMessage, headers);
+        CompletableFuture<String> cf = publisherService.publishToDemoTopic(demoMessage, headers);
+        if(cf.get() != null) {
+            return "Message published, MessageId: "+cf.get();
+        }
+
+        throw new RuntimeException("Failed to publish message \n"+demoMessage);
     }
 }
--- src/main/java/com/balam/pubsub/integration/service/PublisherService.java
+++ src/main/java/com/balam/pubsub/integration/service/PublisherService.java
@@ -4,12 +4,14 @@ import com.balam.pubsub.integration.model.DemoMessage;
 import com.google.cloud.spring.pubsub.core.PubSubTemplate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 @Slf4j
 @Service
@@ -18,13 +20,37 @@ public class PublisherService {
     @Autowired
     PubSubTemplate pubSubTemplate;
 
-    public String  publishToDemoTopic(DemoMessage demoMessage, Map<String, String> headers) throws ExecutionException, InterruptedException {
-        CompletableFuture<String> completableFuture;
+    @Value("${default-client-id}")
+    String defaultClientIdValue;
+
+    @Value("${unwanted-headers}")
+    List<String> unwantedHeaders;
+
+    static final String CLIENT_ID = "client-id";
+    static final String REQUEST_ID = "request-id";
+    static final String DEMO_TOPIC = "demo-topic";
+
+    public CompletableFuture<String>  publishToDemoTopic(DemoMessage demoMessage, Map<String, String> headers) {
+        alterHeaders(headers);
         try{
-            completableFuture = pubSubTemplate.publish("demo-topic", demoMessage, headers);
+            return pubSubTemplate.publish(DEMO_TOPIC, demoMessage, headers);
         } catch (Exception exception) {
             throw new RuntimeException(Arrays.toString(exception.getStackTrace()));
         }
-        return completableFuture.get();
     }
+
+    private void alterHeaders(Map<String, String> headers){
+        for (String header : unwantedHeaders) {
+            headers.remove(header);
+        }
+
+        if(!headers.containsKey(CLIENT_ID)){
+            headers.put(CLIENT_ID, defaultClientIdValue);
+        }
+
+        if(!headers.containsKey(REQUEST_ID)){
+            headers.put(REQUEST_ID, UUID.randomUUID().toString());
+        }
+    }
+
 }
